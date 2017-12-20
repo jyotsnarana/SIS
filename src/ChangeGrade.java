2bd9dbed260a26d4de8f47aecafe69b8b520989c
Parisa Nikzad <parisa.nikzad@yahoo.com>
2017-12-20 00:03:41 -0500

    fix change of grade, ad more validation, make sure to insert numerical value for letter grade

--- src/ChangeGrade.java
+++ src/ChangeGrade.java
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.HashMap;
 
 public class ChangeGrade extends JFrame {
 	private JFrame frame;
@@ -113,26 +114,57 @@ public class ChangeGrade extends JFrame {
 			public void mouseClicked(MouseEvent e) {
 				Connection con = null;
 			Statement stmt = null;
-			if((textField_3.getText().toString().trim().length()== 0) && (textField_2.getText().toString().trim().length()== 0 ))
+			if((textField_3.getText().toString().trim().length()== 0) || (textField_2.getText().toString().trim().length()== 0)  || (textField_1.getText().toString().trim().length()== 0 ) || (textField.getText().toString().trim().length() == 0))
     		{
-    			JOptionPane.showMessageDialog(null, "Fill the fields for updation");	
+    			JOptionPane.showMessageDialog(null, "Fill the fields for updating");
     		}else {
 				try 
 				{
 						con = new SQLConnection().getConnection();
-
+//
 						// Create and execute an SQL statement that returns some data.
 						// TODO remove hardcode value of soen6441 make it dynamic
 //				 		String SQL = "update soen6441 set Grade= '"+ textField_3.getText().toUpperCase() +"' where StudentID = '"+textField_2.getText()+"' ";
-				 		String SQL = "SELECT * FROM student_course sc JOIN course c ON c.Id=sc.CourseId WHERE sc.StudentId= '"+ textField_2.getText() +"' AND c.Semester= '"+textField.getText()+"' AND c.CourseId= '"+ textField_1.getText() +"' AND c.Professor= '"+ currentUser.name +"' ";
-				 		stmt = con.createStatement();
+
+						String SQL = "SELECT * FROM student_course sc JOIN course c ON c.Id=sc.CourseId WHERE sc.StudentId= '"+ textField_2.getText() +"' AND c.Semester= '"+textField.getText()+"' AND c.CourseId= '"+ textField_1.getText() +"' AND c.Professor= '"+ currentUser.name +"' ";
+						stmt = con.createStatement();
+						ResultSet rs = stmt.executeQuery(SQL);
+						if(!rs.isBeforeFirst()){
+							throw new Exception("course_not_valid");
+						}
+
+//						Convert Letter Grade to numerical value
+						HashMap<String, Double> point = new HashMap<String, Double>();
+						point.put("A+", 4.3);
+						point.put("A", 4.0);
+						point.put("A-", 3.7);
+						point.put("B+", 3.3);
+						point.put("B", 3.0);
+						point.put("B-", 2.7);
+						point.put("C+", 2.3);
+						point.put("C", 2.0);
+						point.put("C-", 1.7);
+						point.put("D+", 1.3);
+						point.put("D", 1.0);
+						point.put("D-", 0.7);
+						point.put("F", 0.01);
+
+//						To get the data:
+//						point.get("A+");
+
+						Statement stmt2 = null;
+				 		String SQL2 = "UPDATE student_record SET Grade ='"+ textField_3.getText().toUpperCase() +"', point = "+ point.get(textField_3.getText().toUpperCase()) +" WHERE StudentId= '"+ textField_2.getText() +"' AND Semester= '"+textField.getText()+"' AND CourseId= '"+ textField_1.getText() +"'";
+				 		stmt2 = con.createStatement();
 				 	    System.out.println(textField_3.getText());
-		        		stmt.executeUpdate(SQL);
+		        		stmt2.executeUpdate(SQL2);
 		        		JOptionPane.showMessageDialog(null, "Database updated");
 		        		}
 		       
 				catch (Exception e1)
-				{  
+				{
+					if(e1.getMessage().equals("course_not_valid")) {
+						JOptionPane.showMessageDialog(null, "Check the validity of the data you entered, we can not find a record of that class taken by this student: " + textField_2.getText() + " and given by this Professor: " + currentUser.name);
+					}
 					 JOptionPane.showMessageDialog(null,e1);
 			    }  
 	 		}
